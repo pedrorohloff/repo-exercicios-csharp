020e3e9e36993a2755cd790480938313ba696c5c
Pedro Rohloff <pedro.h.rohloff@gmail.com>
2024-06-26 15:35:03 -0300

    Adicionado exercicios 018 e 019

---
+++ csharpbasicsexercises/ex018/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+18. Write a C# program to check a pair of integers and return true if one is negative and one is positive.
+Sample Output:
+Input first integer:
+-5
+Input second integer:
+25
+Check if one is negative and one is positive:
+True 
+*/
+
+Console.WriteLine("Input first integer:");
+int fisrtInteger = (int.Parse(Console.ReadLine()));
+
+Console.WriteLine("Input second integer:");
+int secondInteger = (int.Parse(Console.ReadLine()));
+
+if (fisrtInteger > 0 && secondInteger < 0)
+{
+    Console.WriteLine("True");
+}
+else if (fisrtInteger < 0 && secondInteger > 0)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+
---
+++ csharpbasicsexercises/ex019/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+19. Write a C# program to compute the sum of two given integers. If two values are the same, return the triple of their sum. 
+*/
+
+System.Console.WriteLine("Input fisrt integer:");
+int firstNumber = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Input second integer:");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine(firstNumber == secondNumber ? firstNumber * 3 : firstNumber + secondNumber);
+
+
+
+
+
+
+
+
+
+
+
+
